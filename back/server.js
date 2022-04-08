08daa9034a881daf5f33236628cee11d5ed8f051
Steven Bernamont <stevenbernamontcontact@gmail.com>
2022-04-08 15:24:19 +0200

    added dbconfig & new .env

--- back/server.js
+++ back/server.js
@@ -2,14 +2,16 @@ var express = require('express');
 
 var app = express();
 
-require('dotenv').config()
+require('dotenv').config();
+
+const models = require('./app/models');
 
 app.get("/", (req, res) => {
 	res.json({ message: "Welcome to Groupomania application." });
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}.`);
 });
