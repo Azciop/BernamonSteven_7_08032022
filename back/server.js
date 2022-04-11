25b08003eac2f6a0e380367817ecb05954c2b4b3
Steven Bernamont <stevenbernamontcontact@gmail.com>
2022-04-11 14:57:25 +0200

    test start server

--- back/server.js
+++ back/server.js
@@ -1,15 +1,20 @@
+const http = require('http');
 var express = require('express');
 
 var app = express();
 
 require('dotenv').config();
 
-const models = require('./app/models');
+const db = require('./app/models/');
 
 app.get("/", (req, res) => {
 	res.json({ message: "Welcome to Groupomania application." });
 });
 
+db.sequelize.sync({ force: false }).then(() => {
+	console.log("Drop and re-sync db.");
+});
+const server = http.createServer(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
